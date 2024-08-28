366235261377c2adaaec33a3e40a1609a145fb0c
Elias Joel Hossein <joelhossein@gmail.com>
2024-08-28 09:20:19 -0400

    first commit

---
+++ Formulario_Busqueda.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Buscador_Telefonos
+{
+    public partial class Formulario_Busqueda : Form
+    {
+        public Formulario_Busqueda()
+        {
+            InitializeComponent();
+        }
+
+        private string consulta_sql(string documento, string email)
+        {
+            string query;
+            query = "SELECT DISTINCT NRO_DOCUMENTO, EMAIL FROM CANALES_EMAILS WHERE";
+
+            if (documento != "")
+            {
+                if (email != "")
+                {
+                    query += " NRO_DOCUMENTO = " + documento + " AND EMAIL = '" + email + "'";
+                }
+                else
+                {
+                    query += " NRO_DOCUMENTO = " + documento;
+                }
+            }
+            else
+            {
+                if (email != "")
+                {
+                    query += " EMAIL = '" + email + "'";
+                }
+                else
+                {
+                    query += "";
+                }
+            }
+            return query;
+        }
+
+        private void btn_buscar_Click(object sender, EventArgs e)
+        {
+            string documento = txt_documento.Text;
+            string email = txt_email.Text.ToLower();
+
+            // Validar entradas de TextBox
+            // 1) Valido que el valor de los campos (documento numérico y email texto)
+            if ((!documento.All(char.IsNumber) && documento != "") || (email.All(char.IsNumber) && email != ""))
+            {
+                MessageBox.Show("Algunos de los campos ingresados no son válidos");
+                return;
+            }
+            // 2) Valido que el email sea contenga @
+            if (email != "" && !email.Contains("@"))
+            {
+                MessageBox.Show("El email ingresado no es válido");
+                return;
+            }
+            // 3) Valido que los campos no sean todos vacíos
+            if (documento == "" && email == "")
+            {
+                MessageBox.Show("Debe ingresar al menos 1 campo de Búsqueda");
+                return;
+            }
+
+            string query = consulta_sql(documento, email);
+
+            // Creo tabla en nuevo formulario y realizo conexión SQL
+            Tabla resultado = new Tabla();
+            string conexionSql = "Data Source=CSPMA000DW02;Initial Catalog=DMCOBRANZAS;Integrated Security=True";
+            var conexion = new SqlConnection(conexionSql);
+            try
+            {
+                conexion.Open();
+                var adaptador = new SqlDataAdapter(query, conexion);
+                var ds = new DataSet();
+                adaptador.Fill(ds);
+                resultado.dgv1.ReadOnly = true;
+                resultado.dgv1.DataSource = ds.Tables[0];
+
+                resultado.lbl_info.Text = "(" + resultado.dgv1.ColumnCount + " Campos, " + resultado.dgv1.RowCount + " Registros)";
+
+                resultado.Show();
+                conexion.Close();
+            }
+            catch
+            {
+                MessageBox.Show("No fue posible conectar a la base de Datos. Verifique sus permisos.");
+            }
+        }
+
+        private void txt_KeyPress_Enter(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                btn_buscar_Click(sender, e);
+            }
+        }
+        private void txt_KeyPress_Escape(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)27)
+            {
+                this.Close();
+            }
+        }
+
+        private void btn_salir_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btn_limpiar_Click(object sender, EventArgs e)
+        {
+            txt_documento.Text = string.Empty;
+            txt_email.Text = string.Empty;
+        }
+    }
+}
---
+++ Formulario_Busqueda.designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Buscador_Telefonos
+{
+    partial class Formulario_Busqueda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Formulario_Busqueda));
+            this.lbl_documento = new System.Windows.Forms.Label();
+            this.lbl_email = new System.Windows.Forms.Label();
+            this.txt_documento = new System.Windows.Forms.TextBox();
+            this.txt_email = new System.Windows.Forms.TextBox();
+            this.btn_buscar = new System.Windows.Forms.Button();
+            this.btn_limpiar = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.toolTip_buscar = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lbl_documento
+            // 
+            this.lbl_documento.AutoSize = true;
+            this.lbl_documento.Location = new System.Drawing.Point(40, 16);
+            this.lbl_documento.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_documento.Name = "lbl_documento";
+            this.lbl_documento.Size = new System.Drawing.Size(77, 13);
+            this.lbl_documento.TabIndex = 0;
+            this.lbl_documento.Text = "DOCUMENTO";
+            // 
+            // lbl_email
+            // 
+            this.lbl_email.AutoSize = true;
+            this.lbl_email.Location = new System.Drawing.Point(224, 16);
+            this.lbl_email.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lbl_email.Name = "lbl_email";
+            this.lbl_email.Size = new System.Drawing.Size(39, 13);
+            this.lbl_email.TabIndex = 1;
+            this.lbl_email.Text = "EMAIL";
+            // 
+            // txt_documento
+            // 
+            this.txt_documento.Location = new System.Drawing.Point(40, 40);
+            this.txt_documento.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_documento.Name = "txt_documento";
+            this.txt_documento.Size = new System.Drawing.Size(115, 20);
+            this.txt_documento.TabIndex = 1;
+            this.txt_documento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress_Escape);
+            this.txt_documento.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress_Enter);
+            // 
+            // txt_email
+            // 
+            this.txt_email.Location = new System.Drawing.Point(224, 40);
+            this.txt_email.Margin = new System.Windows.Forms.Padding(2);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(115, 20);
+            this.txt_email.TabIndex = 2;
+            this.txt_email.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress_Escape);
+            this.txt_email.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress_Enter);
+            // 
+            // btn_buscar
+            // 
+            this.btn_buscar.Location = new System.Drawing.Point(40, 96);
+            this.btn_buscar.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_buscar.Name = "btn_buscar";
+            this.btn_buscar.Size = new System.Drawing.Size(104, 32);
+            this.btn_buscar.TabIndex = 4;
+            this.btn_buscar.Text = "Buscar";
+            this.toolTip_buscar.SetToolTip(this.btn_buscar, "Enter");
+            this.btn_buscar.UseVisualStyleBackColor = true;
+            this.btn_buscar.Click += new System.EventHandler(this.btn_buscar_Click);
+            this.btn_buscar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress_Escape);
+            // 
+            // btn_limpiar
+            // 
+            this.btn_limpiar.Location = new System.Drawing.Point(40, 160);
+            this.btn_limpiar.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_limpiar.Name = "btn_limpiar";
+            this.btn_limpiar.Size = new System.Drawing.Size(104, 32);
+            this.btn_limpiar.TabIndex = 6;
+            this.btn_limpiar.Text = "Limpiar";
+            this.btn_limpiar.UseVisualStyleBackColor = true;
+            this.btn_limpiar.Click += new System.EventHandler(this.btn_limpiar_Click);
+            this.btn_limpiar.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress_Escape);
+            // 
+            // btn_salir
+            // 
+            this.btn_salir.Location = new System.Drawing.Point(232, 160);
+            this.btn_salir.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(104, 32);
+            this.btn_salir.TabIndex = 7;
+            this.btn_salir.Text = "Salir";
+            this.toolTip_buscar.SetToolTip(this.btn_salir, "Salir (Esc)");
+            this.btn_salir.UseVisualStyleBackColor = true;
+            this.btn_salir.Click += new System.EventHandler(this.btn_salir_Click);
+            this.btn_salir.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress_Escape);
+            // 
+            // Formulario_Busqueda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 224);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_limpiar);
+            this.Controls.Add(this.btn_buscar);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_documento);
+            this.Controls.Add(this.lbl_email);
+            this.Controls.Add(this.lbl_documento);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Formulario_Busqueda";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Buscador de Emails";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txt_KeyPress_Escape);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl_documento;
+        private System.Windows.Forms.Label lbl_email;
+        private System.Windows.Forms.TextBox txt_documento;
+        private System.Windows.Forms.TextBox txt_email;
+        private System.Windows.Forms.Button btn_buscar;
+        private System.Windows.Forms.Button btn_limpiar;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.ToolTip toolTip_buscar;
+    }
+}
+
